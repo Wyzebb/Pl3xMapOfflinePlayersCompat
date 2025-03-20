e827f5e66fc8ee89e7ddcca54c415c67d1eb86fb
Wyzebb <BenSJSmith8@gmail.com>
2025-03-20 18:17:24 +0000

    Initial commit

---
+++ src/main/java/me/wyzebb/pl3xMapOfflinePlayersCompat/Pl3xMapOfflinePlayersCompat.java
@@ -0,0 +1,17 @@
+package me.wyzebb.pl3xMapOfflinePlayersCompat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pl3xMapOfflinePlayersCompat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
